5c581d3e2c6e3df8a155d00ac5ba74ac31903001
jparevalo <jparevalo@miuandes.cl>
2017-06-05 12:09:24 -0400

    [Test #3] Accept a roll with 10 knocked pins

--- BowlingGame/game.py
+++ BowlingGame/game.py
@@ -1,7 +1,7 @@
 
 class Game(object):
     def record_roll(self, num_pins_knocked):
-        if num_pins_knocked not in range(0,10):
+        if num_pins_knocked not in range(0, 11):
             raise InvalidNumberError("The given number must be between 0 and 10")
 
 
